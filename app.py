e5b9f94e811bed32e370caf52e6756b2c5078ae5
Aesha <aeshashah2634@gmail.com>
2023-03-12 08:39:05 +0530

    Updated the model

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template, request, redirect
-from flask_pymongo import PyMongo
-from datetime import datetime
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/dynamic_coupon', methods = ["POST"])
-def home():
-    if request.method == "POST":
-        # accept the product and then give the discount
-        data = request.form
-        print(data)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def hello_world():
+    return render_template("forest_fire.html")
 
 
-if (__name__ == "__main__"):
-    app.run(debug = True)
+@app.route('/predict',methods=['POST','GET'])
+def predict(x):
+    int_feature = int(x)
+    final = [np.array(int_feature)]
+    print(int_feature)
+    print(final)
+    prediction = model.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+    return output
+
+if __name__ == '__main__':
+    app.run(debug=True)
